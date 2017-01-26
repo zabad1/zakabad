1351df53ce274204c935f911dd11526d6a168a05
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-25 20:52:13 -0700

    corrected use case for jerry chavez

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,21 +9,23 @@
 			<h3>Audience</h3>
 				<p>This site is aimed at web development companies and also be able to show non-web IT companies.</p>
 			<h3>Goal</h3>
-				<p>A site that shows what I look like as a person and what I have done in my work history and web developed projects and so that companies can see what I have done and can get my contact info off my website.</p>
+				<p>A site that shows what I look like as a person and what I have done in my work history and web developed projects. So companies can see what I have done and can get my contact info off my website.</p>
 			<h2>Persona</h2>
-				<p>Name:Jerry Chavez</p>
-				<p>Age:31</p>
+				<p>Name: Jerry Chavez</p>
+				<p>Age: 31</p>
 				<p>Occupation: Freelance Artist </p>
-				<p>Technology:android, HP pavilion</p>
-				<p>Attitudes and Behaviors:Jerry is a graffiti style artist. He works for UPS as a side job to get his benefits and for a little extra cash. He watches his daughter when he gets home from work at early in morning around 7:30am to 8:00, because his UPS work time starts at 4:45. When he gets home, his wife goes to work and the rest of the day he has family/dad duties to do for his wife and daughter. Jerry knows how to use basic technology at a basic level, has no desire to go further with it. But is seeking someone that can make him a website to show off his art.</p>
-				<p>Frustrations and Needs: Jerry is needing a person that can build him a website, but is very poor and can't afford the average price range that it takes to hire someone to build a website to show off his art. He is willing to do some kind of freelance trade, so he can get a website for his work. </p>
-				<p>Goals: Jerry just needs a simply website with maybe 5 pages and show off his art in the most professional way possible.</p>
+				<p>Technology: android, HP pavilion</p>
+				<p>Attitudes and Behaviors: Jerry is a graffiti style artist. He works for UPS as a side job to get his benefits and for a little extra cash. He watches his daughter when he gets home from work at early in morning around 7:30am to 8:00, because his UPS work time starts at 4:45am. When he gets home, his wife goes to work and the rest of the day he has family/dad duties to do for his wife and daughter. Jerry knows how to use basic technology at a basic level, has no desire to go further with it. But is seeking someone that can make him a website to show off his art.</p>
+				<p>Frustrations and Needs: Jerry is needing a person that can build him a website, but is very poor and can't afford the average price range that it takes to hire someone to build a website to show off his art. He is willing to do some kind of freelance trade, so he can get a website for his art work. </p>
+				<p>Goals: Jerry just needs a simply website with maybe 5 pages and shows off his art in the most professional way possible.</p>
 			<h2>Use Case</h2>
 			<ol>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
+				<li>Jerry gets on his android device and looks up freelance website developers that can make him a site in his local area.</li>
+				<li>He finds a few and looks through all their work. Decides which one he likes the best and can also possible make a freelance trade deal with.</li>
+				<li>Jerry sends an email regarding if he can make a deal with the web developer, so he doesn't waste his time right away if they meet up. And he also states that if he is willing to do a trade, if they can set up a time and place to discuss questions that he has.</li>
+				<li>Jerry receives an email back. The web developer is willing to work a trade, if he can see his work first as an artist. Web guy says he can meet up at Satellite coffee at 2pm and talk business.</li>
+				<li>Jerry confirms with the email about the time and location.</li>
+				<li>They meet up with each other and Jerry discuss's what he is looking for in a website and also shows his art to the web guy to see if he is interested in the trade. The web guy agrees to the trade. They both discuss what is considered equal for a trade off. They come to agree to terms and set a deadline of when they both can have the work done for each other.</li>
 			</ol>
 		</body>
 </html>
