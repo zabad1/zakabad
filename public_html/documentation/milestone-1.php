a37b034e0a816be0b9b537b7214bd97924c26dc9
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-25 17:09:17 -0700

    added old milestone 1 doc from archive

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Zak Abad Resume site</title>
+	</head>
+	Personal website about my self/achevements and work history for of my resume.
+		<body>
+			<h1>Zak Abad</h1>
+			<p>Age:32</p>
+			<p>Occupation: UPS supervisor</p>
+			<p>Technology Education: Android, iPhone, iMac, PC</p>
+			<p>Purpose</p>
+			<p>Businesses that I apply to will see a site that is about me and whatever info they need to contact me. Any
+			any demographic can look at my site, but it will be only for business use purposes.It will only need wireframing for </p>
+			<p>Goals: A site that says a little bit about my personal self and also has info for my work history that I have
+			done in the past and what I am doing currently for web developing. </p>
+			<p>Frustrations/Additional information: The only frustrations I will have just remembering all the coding for building the site. Additional thing I want to do for my site is, have a quote from someone that you see above my profile picture, but I want the quote to change if you refresh the site or go back another time. So the end user will always see different quote every time they go to my website.</p>
+			<h1>Use Case</h1>
+			<ol>
+				<li>I give the persona at Satellite cafe at 4:00pm my resume or business card of basic info of myself, but has my website URL on it so they can see more things about myself.</li>
+				<li>They decide to look me up on the web because I came off like a nice/smart person that could be a person that could work for them.</li>
+				<li>It can be accessed through mobile device or desktop/laptop device. This persona has their macbookPro with them and types in my URL.</li>
+				<li>The persona has the goal of trying to hire a person that has life experience and work life experience. And that's what you'll be able to see with my website. </li>
+			</ol>
+		</body>
+</html>
