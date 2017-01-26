dd0d8db6eabe8d2931f567e878b527fbf7d653d8
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-25 20:29:44 -0700

    added/corrected all persona attributes.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,17 +9,16 @@
 			<h3>Audience</h3>
 				<p>This site is aimed at web development companies and also be able to show non-web IT companies.</p>
 			<h3>Goal</h3>
-				<p></p>
-			<h1>Persona</h1>
-			<p>Name:</p>
-			<p>Age:</p>
-			<p>Occupation: </p>
-			<p>Technology:</p>
-			<p>Attitudes and Behaviors:</p>
-			<p>Frustrations and Needs:</p>
-			<p>Goals:</p>
-			<p></p>
-			<h1>Use Case</h1>
+				<p>A site that shows what I look like as a person and what I have done in my work history and web developed projects and so that companies can see what I have done and can get my contact info off my website.</p>
+			<h2>Persona</h2>
+				<p>Name:Jerry Chavez</p>
+				<p>Age:31</p>
+				<p>Occupation: Freelance Artist </p>
+				<p>Technology:android, HP pavilion</p>
+				<p>Attitudes and Behaviors:Jerry is a graffiti style artist. He works for UPS as a side job to get his benefits and for a little extra cash. He watches his daughter when he gets home from work at early in morning around 7:30am to 8:00, because his UPS work time starts at 4:45. When he gets home, his wife goes to work and the rest of the day he has family/dad duties to do for his wife and daughter. Jerry knows how to use basic technology at a basic level, has no desire to go further with it. But is seeking someone that can make him a website to show off his art.</p>
+				<p>Frustrations and Needs: Jerry is needing a person that can build him a website, but is very poor and can't afford the average price range that it takes to hire someone to build a website to show off his art. He is willing to do some kind of freelance trade, so he can get a website for his work. </p>
+				<p>Goals: Jerry just needs a simply website with maybe 5 pages and show off his art in the most professional way possible.</p>
+			<h2>Use Case</h2>
 			<ol>
 				<li></li>
 				<li></li>
