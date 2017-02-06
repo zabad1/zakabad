623b52e138b442d1009850d0620b5637247a67f5
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-02-06 14:58:13 -0700

    corrected images

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,14 +6,8 @@
 			<title>Zak Abad Resume site</title>
 		</head>
 		<body>
-			<p>I want the website to have lines of sentences that says things about my self. Links to my github, twitter,
-			Linkin accounts. A picture of my self, with a gradinent blue background. A contact me on the front page and a
-			second page with links of websites I've done and another contact me message. An easter egg on my picture. I
-			want to have a quote from 'x' person, but I want it to be programed to be randomized so I can have many different
-			quotes. So when you go to the website every time there is a new quote there.</p>
-			<img src = "/images/destop%20laptop%20layout%20website.jpg"><br>
-			<img src= "/images/second%20pg%20desktop%20laptop%20lay%20out.jpg"><br>
-			<img src = "/images/mobile%20site.jpg"><br>
-			<img src = "/images/mobile%20layout%202pg.jpg"><br>
+			<p></p>
+			<img src="images/mobilelayout.jpg" height="650" width="450">
+			<img src="images/destoplayout.jpg" height="450" width="750">
 		</body>
-	</html>
+	</html
