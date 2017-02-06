a5555b0c49ffecd02952615b1b4a86044bd804b6
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-02-06 15:00:40 -0700

    updated <p>

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,7 +6,9 @@
 			<title>Zak Abad Resume site</title>
 		</head>
 		<body>
-			<p></p>
+			<p>There is goint to be an about me sentences. There will be two links, one to wife's photography website and
+			the other to sprout swap website. Contact me section. Links to my twitter, github, and linkin. Easter egg on
+			my picture.</p>
 			<img src="images/mobilelayout.jpg" height="650" width="450">
 			<img src="images/destoplayout.jpg" height="450" width="750">
 		</body>
