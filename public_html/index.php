642d1bc16bc5649aaaf9eb46715fba00de120b6e
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-02-28 16:44:15 -0700

    made adjustments to many areas

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,15 @@
 <html lang="en">
 		<head>
 			<meta charset="UTF-8">
-
+*//TODO:
+			Footer color/
+			Moblie Responsive/
+			About me/
+			Recaptcha|Send email/
+			separate tab for links in nav/
+			Add Favicon|create/
+			Change stripe to blocks?/
+			<!--Bootstrap nav bar-->
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 			<script
@@ -12,16 +20,29 @@
 
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+			<!--Recaptcha api-->
 			<script src='https://www.google.com/recaptcha/api.js'></script>
 
-			<link rel="stylesheet" href="zakabad.css">
+			<!--CSS call tag sheet-->
+			<link rel="stylesheet" href="css/zakabad.css">
 
+			<!--Google Fonts-->
 			<link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+			<link href="https://fonts.googleapis.com/css?family=Rokkitt" rel="stylesheet">
 
-			<title>Zak Abad </title>
+<!--Favicon image icon-->
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+			<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+			<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+			<link rel="manifest" href="/manifest.json">
+			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+			<meta name="theme-color" content="#ffffff">
+
+			<title>Zak Abad</title>
 		</head>
 
 		<body>
+			<!--Navbar-->
 			<nav class="navbar navbar-inverse">
 				<!-- logo and mobile toggle button get grouped together for better mobile display -->
 				<div class="navbar-header">
@@ -50,6 +71,7 @@
 			<div class="bg-2 text-center">
 				<div class="container-fluid">
 
+					<!--About me-->
 					<h3 class="margin">Who Am I?</h3>
 					<div class="colorBlock1"><p class="textBlock1">My main focus is front end web developer. Though I do enjoy expanding my knowledge in all phases of programming. When I tackle anything that requires a lot of learning, I don't shy away from learning everything I can, so it can make me more reliable in my self.
 						</p></div>
@@ -60,7 +82,7 @@
 					<h3 class="margin">Life</h3>
 					<div class="colorBlock3"><p class="textBlock3">fjkflkjawoigfejaokdsljflkjfdklsajfk ljdaslkjflkjadsgjda;jladsjfjdasgkdfls jgldsjaglk;jasdgkljl kadgsjlkgjdsla 	kjgalksljkfdsljkfhsdkja hfadsfkdskljhfkashjkd fsaklfdlkasf;ldsfj;ljfd;jfjfkldsak f;ljsad;klfjk;l sadjf;sdl ;fkjsdl fjk;ldsjkf;dsa ;ffd;klsjfkl;sjalfj</p></div>
 
-					<!--Contact me starts-->
+					<!--Contact me form starts-->
 							<div class="form-container ng-scope">
 								<form id="contact-form" action="php/mailer/mailer.php" method="post" novalidate="novalidate" class="ng-pristine ng-valid">
 									<h3 id="contact-heading">Contact Me</h3>
@@ -116,6 +138,7 @@
 				</div>
 			</div>
 
+<!--Footer starts here-->
 			<footer class="navbar" id="bottomBoy"></footer>
 		</body>
 	</html>
