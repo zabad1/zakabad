fc2480b30c92816b7050de0dd1dfbf9152ce7a9c
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-01 09:11:04 -0700

    added shawdow to pic

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,7 @@
 			<meta charset="UTF-8">
 *//TODO:
 			Footer color/
-			Moblie Responsive/
+			Moblie Responsive & fix footer for mobile/
 			About me/
 			Recaptcha|Send email/
 			separate tab for links in nav/
@@ -30,10 +30,10 @@
 			<link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
 			<link href="https://fonts.googleapis.com/css?family=Rokkitt" rel="stylesheet">
 
-<!--Favicon image icon-->
+			<!--Favicon image icon-->
 			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-			<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-			<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+			<link rel="icon" type="image/png" href="documentation/images/celluloidFilmFavicon4.png" sizes="32x32">
+			<link rel="icon" type="image/png" href="documentation/images/celluloidFilmFavicon4.png" sizes="16x16">
 			<link rel="manifest" href="/manifest.json">
 			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
 			<meta name="theme-color" content="#ffffff">
