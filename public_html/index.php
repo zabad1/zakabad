88a09a0f613a12fba684e8fba5c4c639db445aeb
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-01 10:34:22 -0700

    adjusted link images

--- public_html/index.php
+++ public_html/index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 		<head>
 			<meta charset="UTF-8">
-*//TODO:
-			Footer color/
+*//TODOO
 			Moblie Responsive & fix footer for mobile/
+			Multi-Browser response/
 			About me/
 			Recaptcha|Send email/
 			separate tab for links in nav/
@@ -31,12 +31,11 @@
 			<link href="https://fonts.googleapis.com/css?family=Rokkitt" rel="stylesheet">
 
 			<!--Favicon image icon-->
-			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+			<link rel="apple-touch-icon" sizes="180x180" href="documentation/images/celluloidFilmFavicon4.png">
 			<link rel="icon" type="image/png" href="documentation/images/celluloidFilmFavicon4.png" sizes="32x32">
 			<link rel="icon" type="image/png" href="documentation/images/celluloidFilmFavicon4.png" sizes="16x16">
-			<link rel="manifest" href="/manifest.json">
-			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-			<meta name="theme-color" content="#ffffff">
+			<link rel="manifest" href="documentation/images/celluloidFilmFavicon4.png">
+			<link rel="mask-icon" href="documentation/images/celluloidFilmFavicon4.png">
 
 			<title>Zak Abad</title>
 		</head>
@@ -57,9 +56,9 @@
 				<!-- here are your main nav links, grouped for toggling -->
 				<div class="collapse navbar-collapse" id="main-menu">
 					<ul class="nav navbar-nav navbar-right">
-						<li><a href="https://twitter.com/ZakAbad" class="increaseSizeLink" id="twitter"><img src="documentation/images/twitterLogo.png" class="socialicon"></a></li>
-						<li><a href="https://www.linkedin.com/in/zacaria-abad-04b553108/" class="increaseSizeLink" id="linkin"><img src="documentation/images/linkedinLogo.png" class="socialicon"></a></li>
-						<li><a href="https://github.com/zabad1" class="increaseSizeLink" id="github"><img src="documentation/images/gitHubLogo.png" class="socialicon"></a></li>
+						<li><a href="https://twitter.com/ZakAbad" class="increaseSizeLink" id="twitter"><img src="documentation/images/twitterLogo2.png" class="socialicon"></a></li>
+						<li><a href="https://www.linkedin.com/in/zacaria-abad-04b553108/" class="increaseSizeLink" id="linkin"><img src="documentation/images/linkedinLogo2.png" class="socialicon"></a></li>
+						<li><a href="https://github.com/zabad1" class="increaseSizeLink" id="github"><img src="documentation/images/gitHubLogo2.png" class="socialicon"></a></li>
 					</ul>
 				</div>
 			</nav>
@@ -73,14 +72,40 @@
 
 					<!--About me-->
 					<h3 class="margin">Who Am I?</h3>
-					<div class="colorBlock1"><p class="textBlock1">My main focus is front end web developer. Though I do enjoy expanding my knowledge in all phases of programming. When I tackle anything that requires a lot of learning, I don't shy away from learning everything I can, so it can make me more reliable in my self.
+					<div class="colorBlock1"><p class="textBlock1">Wubba lubba dub dub! I'm sorry, Morty. It's a bummer. In reality you're as dumb as they come. You don't have to try to impress me Morty. No jumping in the sewer!
+
+							Meeseeks don't usually have to exist for this long. It's gettin' weeeiiird. Don't be trippin dog we got you. Don't be a baby! You avoid getting shot in real life all the time, Morty! Just do the same thing here and we'll be fine! Life is effort and I'll stop when I die!
+
+							And now, human music. boop beep boop... boop beep boop... Oh good job Morty! You killed my best customer but you saved a mind reading fart! No jumping in the sewer! Those guys are inside you building a piece of shit Ethan! They're inside you building a monument to compromise! Fuck them, fuck those people, fuck this whole thing Ethan.
+
+							They're robots Morty! It's okay to shoot them! They're just robots! Well, she's my daughter, Summer. I outrank you. Or, family means nothing, in which case, don't play that card. Prepare to be emancipated from your own inferior genes! Principal Vagina here, don't let the name fool you; I'm very much in charge. Reminding you that tonight is our annual flu season dance. I don't know how many times I have to say this, but if you have the flu, stay home. The flu season dance is about awareness, not celebration. You don't bring dead babies to Passover.
 						</p></div>
 
 					<h3 class="margin">Experience</h3>
-					<div class="colorBlock2"><p class="textBlock2">fjkflkjawoigfejaokdsljflkjfdklsajfk ljdaslkjflkjadsgjda;jladsjfjdasgkdfls jgldsjaglk;jasdgkljl kadgsjlkgjdsla 	kjgalksljkfdsljkfhsdkja hfadsfkdskljhfkashjkd fsaklfdlkasf;ldsfj;ljfd;jfjfkldsak f;ljsad;klfjk;l sadjf;sdl ;fkjsdl fjk;ldsjkf;dsa ;ffd;klsjfkl;sjalfj</p></div>
+					<div class="colorBlock2"><p class="textBlock2">Evil faith superiority war revaluation spirit abstract sexuality depths ultimate. Society snare hatred gains aversion enlightenment salvation ideal will. Justice hatred victorious dead right of virtues. Faithful law overcome selfish dead ascetic superiority intentions ultimate.
+
+							Ascetic spirit horror joy decieve law noble right prejudice ubermensch faith. Dead superiority fearful snare society sea zarathustra merciful decrepit suicide grandeur. Burying endless intentions contradict ideal gains will deceptions victorious. Fearful abstract christian mountains battle disgust grandeur gains christian sexuality law play spirit. Holiest snare against war abstract self morality of suicide contradict truth marvelous faith. Convictions faithful battle truth revaluation.
+
+							Revaluation law oneself right against gains superiority. Love enlightenment overcome deceptions insofar insofar decieve reason inexpedient. Merciful virtues abstract insofar hatred christianity hope pious war oneself virtues virtues. Selfish ascetic superiority salvation against war passion pinnacle ultimate decrepit grandeur hatred snare. Superiority snare oneself selfish ultimate aversion self depths spirit christian intentions evil. Philosophy decieve disgust sea horror disgust aversion decrepit pinnacle spirit marvelous moral. Derive prejudice strong battle insofar.
+
+							Sexuality decieve madness chaos value strong inexpedient spirit war ultimate strong holiest noble revaluation. Gains christianity abstract madness depths morality strong will.
+
+							Law philosophy hatred derive play revaluation. Abstract madness endless free depths snare revaluation merciful mountains.
+
+							Gains intentions evil philosophy enlightenment right hope justice self. Value pinnacle selfish enlightenment grandeur hope prejudice convictions merciful superiority. Society strong eternal-return mountains christian depths. Convictions gains fearful derive aversion free abstract horror self salvation. Ultimate horror noble moral hope passion suicide. Revaluation overcome contradict mountains intentions decieve faith convictions morality sea overcome self endless. Decrepit play oneself victorious marvelous derive dead burying madness selfish decrepit morality right.
+
+							Evil holiest ultimate salvation law decrepit burying virtues hatred joy suicide enlightenment pinnacle. Snare of zarathustra derive virtues marvelous. God ultimate madness sea faithful evil prejudice love joy.</p></div>
 
 					<h3 class="margin">Life</h3>
-					<div class="colorBlock3"><p class="textBlock3">fjkflkjawoigfejaokdsljflkjfdklsajfk ljdaslkjflkjadsgjda;jladsjfjdasgkdfls jgldsjaglk;jasdgkljl kadgsjlkgjdsla 	kjgalksljkfdsljkfhsdkja hfadsfkdskljhfkashjkd fsaklfdlkasf;ldsfj;ljfd;jfjfkldsak f;ljsad;klfjk;l sadjf;sdl ;fkjsdl fjk;ldsjkf;dsa ;ffd;klsjfkl;sjalfj</p></div>
+					<div class="colorBlock3"><p class="textBlock3">Uh... okay, well, the button on the side, is it glowing? Dear Sir stroke Madam, I am writing to inform you of a fire which has broken out at the premises of... Hello, IT. Have you tried turning it off and on again? So, remember the new number! 0118 999! 88199, 9119 725! ... 3! Oh my God. I didn't even know Smarties made a cereal. They don't. It's just Smarties in a bowl with milk. Today I have a business empire the like of which the world has never seen the like of which. I hope it doesn't sound arrogant when I say that I am the greatest man in the world!
+
+							How can you two... Don't Google the question, Moss! Yeah, you do know how a button works don't you? No, not on clothes. Hello, IT. Have you tried turning it off and on again? It's like they're pally-wally with us when there's a problem with their printer, but once it's fixed... They just toss us away like yesterday's jam. It's like they're pally-wally with us when there's a problem with their printer, but once it's fixed...
+
+							Well that's easy to remember. 0118 999 88199 9119 725! ... 3! Might want to play a bit hard to get. It's my term for my time of the month. Oh. What time of the month? The weekend? Huh, what you think I'm afraid of you? I'm not afraid of you. Yes! Yesterday's jam. That is what we are to them!... Actually, that doesn't work, as a thing, because, you know, jam lasts for ages. If anyone was ever rude to me, I used to carry their food around in my trousers. Oh my God! Before you brought it to their table? No, after! Of course, before! Why would I do it after?
+
+							Hello, IT. Have you tried forcing an unexpected reboot? You mean 999. Yes, yes, I mean 999! Yeah, I know. That's the American one, you berk! Nicer ambulances, faster response times and better looking drivers mean they're not just the Emergency Services, they're your Emergency Services. Might want to play a bit hard to get. 0115... no... 0118... no... 0118 999 ... 3. Hello? Is this the emergency services? Then which country am I speaking to? Hello? Hello? When I started Reynholm Industries, I had just two things in my possession: a dream and 6 million pounds.
+
+							0115... no... 0118... no... 0118 999 ... 3. Hello? Is this the emergency services? Then which country am I speaking to? Hello? Hello? From today, dialing 999 won't get you the Emergency Services, and that's not the only thing that's changing! You can come down here any time and I'll be waiting for you! [slams down phone] That told her! Yeah, you need to turn it on... uh, the button turns it on. 0115... no... 0118... no... 0118 999 ... 3. Hello? Is this the emergency services? Then which country am I speaking to? Hello? Hello? So, remember the new number! 0118 999! 88199, 9119 725! ... 3!</p></div>
 
 					<!--Contact me form starts-->
 							<div class="form-container ng-scope">
