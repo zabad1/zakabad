181268551d6a031bca77f99c38b628daeee2bc7d
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-02 16:25:04 -0700

    fixed img for mobile device

--- public_html/index.php
+++ public_html/index.php
@@ -2,10 +2,7 @@
 <html lang="en">
 		<head>
 			<meta charset="UTF-8">
-<!--TODO:
-			Moblie Responsive/
-			Multi-Browser response/
-			About me/-->
+<!--TODO:About me/-->
 
 			<!--Bootstrap nav bar-->
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
